32332409f2e06002b8f36e0aa3796a2e87828823
mRodneyMo <motokads@gmail.com>
2024-06-20 10:23:32 +0300

    finally

---
+++ .history/webpack.config_20240620082955.js
@@ -0,0 +1,17 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { merge } = require('webpack-merge');
+// const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+// module.exports = async function (env, argv) {
+//   const expoConfig = await createExpoWebpackConfigAsync(env, argv);
+
+//   return merge(expoConfig, {
+//     // Customize your Webpack configuration here
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: './public/index.html', // Path to your HTML template
+//       }),
+//     ],
+//   });
+// };
